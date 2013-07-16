d91eb65036a3beabd671eebe93e93eaea76d05a1
Josh Deare <josh.deare@coreos.com>
2013-07-16 14:52:27 -0700

    started oauth auth for GET request to admin appengine urls

--- auth.go
+++ auth.go
@@ -1,45 +1,63 @@
 package main
 
 import (
-	"code.google.com/p/goauth2/oauth"
-	"code.google.com/p/goauth2/oauth/jwt"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
+	"net/url"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"code.google.com/p/gopass"
+	"encoding/json"
 )
+
+type Token struct {
+	AccessToken string `json:"access_token"`
+	TokenType string `json:"token_type"`
+	ExpiresIn int `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
 
 // newDevClient Returns a new oauth2 authenticated *http.Client which works in appengine dev environments
 // TODO: add if the paths are nil then look at datastore
-func NewToken(pemFilePath string, clientSecretsPath string, s string) (*oauth.Token, error) {
-	keybytes, err := ioutil.ReadFile(pemFilePath)
+func NewToken()	(*Token, error){
+	request_args := url.Values{"response_type": {"code"}, "client_id": {clientId}, "redirect_uri": {"urn:ietf:wg:oauth:2.0:oob"}, "scope": {scope}, }
+	goto_url := fmt.Sprintf("%s?%s", "https://accounts.google.com/o/oauth2/auth", request_args.Encode())
+
+	fmt.Println("Goto this url: ", goto_url)
+	code, err := gopass.GetPass("Paste the code here: ")
+	if err != nil {
+		fmt.Printf("ERR with code %v", err)
+	}
+	token_args := url.Values{"code": {code}, "client_id": {clientId}, "client_secret": {clientSecret}, "redirect_uri": {"urn:ietf:wg:oauth:2.0:oob"}, "grant_type": {"authorization_code"}, }
+	resp, err := http.PostForm("https://accounts.google.com/o/oauth2/token", token_args)
 	if err != nil {
 		return nil, err
 	}
-
-	secretbytes, err := ioutil.ReadFile(clientSecretsPath)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
+	token := new(Token)
+	err = json.Unmarshal(body, token)
+	
+	return token, err
+	
+}
 
-	var config struct {
-		Web struct {
-			ClientEmail string `json:"client_email"`
-			ClientID    string `json:"client_id"`
-			TokenURI    string `json:"token_uri"`
-		}
-	}
-
-	err = json.Unmarshal(secretbytes, &config)
+func AdminTest(tok *Token) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%v?%v", "http://api.core-os.net/admin/v1/metrics", tok.AccessToken), nil)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
 	}
-
-	tok := jwt.NewToken(config.Web.ClientEmail, s, keybytes)
-	tok.ClaimSet.Aud = config.Web.TokenURI
-
-	tokClient := &http.Client{}
-
-	o, err := tok.Assert(tokClient)
-
-	return o, err
+	//req.Header.Set("Authorization", "Bearer "+tok.AccessToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf(string(body))
 }
---
+++ main_temp.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+)
+
+func main()	{
+	token, _ := NewToken()
+	AdminTest(token)	
+}
