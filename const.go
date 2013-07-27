f3cbd5383a8552347ad9c89d4338ea0f7d0ac589
Josh Deare <josh.deare@coreos.com>
2013-07-26 19:38:29 -0700

    switched to panic from os.exit()

--- const.go
+++ const.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"net/url"
-	"os"
-	"net/http"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"github.com/coreos/core-admin/certs"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 // Default URL for the update API, overriden with CORE_UPDATE_URL
 // TODO BP: use api.core-os.net instead but need to override CA certs
 var updateURL = url.URL{Scheme: "https", Host: "api.core-os.net"}
-var tlsTransport = &http.Transport {
+var tlsTransport = &http.Transport{
 	DisableCompression: true,
 }
 
@@ -30,28 +30,24 @@ func init() {
 		updateURL = *envUrl
 	}
 	pool := x509.NewCertPool()
-	
+
 	coreosInetPemBlock, _ := pem.Decode([]byte(certs.CoreOS_Internet_Authority_pem))
 	if coreosInetPemBlock == nil {
-		fmt.Printf("Error: No PEM data found in CoreOS_Internet_Auth")
-		os.Exit(-1)
+		panic("Error: No PEM data found in CoreOS_Internet_Auth")
 	}
 
 	coreosNetPemBlock, _ := pem.Decode([]byte(certs.CoreOS_Internet_Authority_pem))
 	if coreosNetPemBlock == nil {
-		fmt.Printf("Error: No PEM data found in CoreOS_Network_Auth")
-		os.Exit(-1)
+		panic("Error: No PEM data found in CoreOS_Network_Auth")
 	}
 
 	coreosInetAuthCert, err := x509.ParseCertificate(coreosInetPemBlock.Bytes)
 	if err != nil {
-		fmt.Printf("Error: invalid Internet Auth Cert: %s", err.Error())
-		os.Exit(-1)
+		panic(err.Error())
 	}
 	coreosNetAuthCert, err := x509.ParseCertificate(coreosNetPemBlock.Bytes)
 	if err != nil {
-		fmt.Printf("Error: invalid Network Auth Cert: %s", err.Error())
-		os.Exit(-1)
+		panic(err.Error())
 	}
 	pool.AddCert(coreosNetAuthCert)
 	pool.AddCert(coreosInetAuthCert)
