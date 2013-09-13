7f11293584e27bdab7d263c3dbd1a890cc8562a5
Brandon Philips <brandon@ifup.co>
2013-09-13 11:31:07 -0700

    fix(test): fixup the test to use a static payload
    using the README was a bad idea.

--- main_test.go
+++ main_test.go
@@ -13,14 +13,14 @@ func ExampleNewVersion() {
 	*versionM = "fixtures/update.metadata"
 	*versionC = true
 
-	args := []string{"README.md"}
+	args := []string{"fixtures/test-payload"}
 
 	fmt.Printf("%s", newVersionRequestBody(args))
 
 	// Output:
 	// <?xml version="1.0" encoding="UTF-8"?>
 	// <version>
-	//  <app id="app" version="ver" track="track"></app>
-	//  <package name="README.md" size="419" path="/asdf/" sha1sum="ROSqaSsyVOcKZXgmf4BfOZ1ZqBo=" sha256sum="yXbomCsHeoku0Xvh1NPkdW+jNxyP77Y5VAsvzslYVGk=" required="false" MetadataSignatureRsa="VQ7HB6VA9hO4SZqUy6CLHF5LFZ2+qjL3cQDA5AqxsrHbWuAJuvib0uqIjQFjFle0Oo2D5GcbtSFnv6WA3tZSvnKDUF9g2x6diSveJtTqVZA2AGBD8hqOj6xr42aWxcfMsaA7QWY8sLxoALsSn5ZGukz1DpHRBTuSjDAMhUyH78+dOtReVRRDeLu4tSLjhEz0b/5+ZfR+bL5PGxTJJOyU031ybOf0/TMVrCtVKpam0LwA4h74Z++yZA+t+4l19dQGffWs+DwTNXC8FZueePB29LSxMqlNL9JLFfvXvpSGbJJI9gws5bSes6dj6sKMuE5rc/Cl5zMTZeWePLs4Xr9JXg==" MetadataSize="55961"></package>
+	//  <app id="app" version="ver" track="track" canary="true"></app>
+	//  <package name="test-payload" size="31" path="/asdf/" sha1sum="z8WFPWWn4WWK1+BDP041WmVKvI4=" sha256sum="hVNX250U8IWczSnzcwhs3lwt8izqSWGsDrYnfENwt2g=" required="false" MetadataSignatureRsa="VQ7HB6VA9hO4SZqUy6CLHF5LFZ2+qjL3cQDA5AqxsrHbWuAJuvib0uqIjQFjFle0Oo2D5GcbtSFnv6WA3tZSvnKDUF9g2x6diSveJtTqVZA2AGBD8hqOj6xr42aWxcfMsaA7QWY8sLxoALsSn5ZGukz1DpHRBTuSjDAMhUyH78+dOtReVRRDeLu4tSLjhEz0b/5+ZfR+bL5PGxTJJOyU031ybOf0/TMVrCtVKpam0LwA4h74Z++yZA+t+4l19dQGffWs+DwTNXC8FZueePB29LSxMqlNL9JLFfvXvpSGbJJI9gws5bSes6dj6sKMuE5rc/Cl5zMTZeWePLs4Xr9JXg==" MetadataSize="55961"></package>
 	// </version>
 }
